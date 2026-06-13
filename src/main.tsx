694670bf330c7809aa75a081fa781425aa03d3d7
Joel Gratwohl <joel.gratwohl@gmail.com>
2026-06-13 10:55:35 +0200

    Update Analytics and Speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
 
 const rootEl = document.getElementById('root') as HTMLElement;
 
+const tree = (
+  <React.StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </React.StrictMode>
+);
+
 if (rootEl.hasChildNodes()) {
-  ReactDOM.hydrateRoot(rootEl, <React.StrictMode><App /></React.StrictMode>);
+  ReactDOM.hydrateRoot(rootEl, tree);
 } else {
-  ReactDOM.createRoot(rootEl).render(<React.StrictMode><App /></React.StrictMode>);
+  ReactDOM.createRoot(rootEl).render(tree);
 }
